c406ac0dc8b5812772739c808039b3fdd9bb12a0
Peri Barros <peribarros@gmail.com>
2023-11-26 22:24:41 -0300

    alteracoes no teste automatizado

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,23 +1,44 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
-context ('Funcionalidade Login', () => {
+context('Funcionalidade Login', () => {
 
     beforeEach(() => {
-         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     });
 
     afterEach(() => {
         cy.screenshot()
     });
-    
-    it('Deve fazer login com sucesso',() =>{
+
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.page-title').should('contain','Minha conta')
+        cy.get('.page-title').should('contain', 'Minha conta')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
     })
+
+    it('Deve fazer login com sucesso - usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+    })
+
+    it.only('Deve fazer login com sucesso - usando fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log: false}) //log:false - não mostra a senha
+            cy.get('.woocommerce-form > .button').click()
+
+            cy.get('.page-title').should('contain', 'Minha conta')
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+        })
+    });
 
     it('Deve exibir uma mensagem de erro ao inserir usuario inválido', () => {
         cy.get('#username').type('ebac@teste.com')
